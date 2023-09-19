8c30980355b6d8f9ec5463ebe73815ee48e85491
keagan erasmus <keaganerasmus12@gmail.com>
2023-09-19 22:16:41 +1200

    Created a ECS sytem and added our first entities, they are also being rendered

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
-use rltk::{GameState, Rltk};
+use rltk::{GameState, Rltk, VirtualKeyCode, RGB};
+use specs::prelude::*;
+use specs_derive::Component;
+use std::cmp::{max, min};
 
-struct State {}
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: rltk::FontCharType,
+    fg: RGB,
+    bg: RGB,
+}
+
+struct State {
+    ecs: World,
+}
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        ctx.print(1, 1, "Hello Rustacians");
+        let position = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&position, &renderables).join(){
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
     }
 }
 
@@ -14,6 +37,22 @@ fn main() -> rltk::BError {
         .with_title("Rust Like")
         .build()
         .expect("Could not create context!!");
-    let gs = State {};
+    let mut gs = State { ecs: World::new() };
+
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+
+    for i in 0..10 {
+        gs.ecs
+            .create_entity()
+            .with(Position { x: i * 7, y: 20 })
+            .with(Renderable {
+                glyph: rltk::to_cp437('@'),
+                fg: RGB::named(rltk::AQUA),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .build();
+    }
+
     rltk::main_loop(context, gs)
 }
